8b7f7ef2c1943b21c52d1469154a89882885d766
Nishant Shah <nishantcshah13@gmail.com>
2023-04-21 15:53:36 +0530

    init

---
+++ src/docs/basicInfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+    openapi:"3.0.1",
+    info:{
+        version:"1.0.0",
+        title:"Todos",
+        description: "Todos API",
+        contact:{
+            name:"Nishant Shah",
+            email:"nishant@hexafoldtech.com",
+            url:"https://hexafoldtech.com"
+        }
+    }
+}
---
+++ src/docs/servers.js
@@ -0,0 +1,12 @@
+module.exports = {
+    servers:[
+        {
+            url:"http://localhost:4000",
+            description:"Local server"
+        },
+        {
+            url:"http://api.nova.com",
+            description:"Local server"
+        },
+    ]
+}
